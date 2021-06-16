07c661731d339170f0339d74a9db02ee01a4373f
Nanda Yana <nandayana0202@gmail.com>
2021-06-16 12:56:16 +0700

    tambah js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function floatingButton() {
+    document.getElementById("tabel").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+    if (!event.target.matches('.section')) {
+      var dropdowns = document.getElementsByClassName("model-adminmanagerdisplay");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
       }
     }
   }
-}
+
+  
