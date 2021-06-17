819451ae75ca0d7ffd4cd0a1718ec8d0d44cdce0
Nanda Yana <nandayana0202@gmail.com>
2021-06-17 17:33:14 +0700

    update lagi

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# javascript
+Less documentation :
+{
+    "watchFolder": "<input_folder>",   
+    "outputFolder": "<output_folder>",
+    "mainFile": "<main-file>",   
+    "sourceMap": false,
+    "plugins": "plugin1,plugin2",
+    "lessArgs": "option1=1,option2=2",
+    "runOnce": false,
+    "enableJs": true
+}
+
+github documentation :
+~ git init
+~ git add .
+~ git commit -m “namabebas”
+~ git status 
+~ git remote add origin diikuti url repo github ,
+Contoh: git remote add origin https://github.com/didikprabowo/html-list.git
+~ git push -u origin master
+
+Git init, untuk memberitahukan repo secara lokal
+Git add , digunakan untuk memasukan inisiasi semua file yang akan di upload
+Git commit, digunakan sebagai penanda
+Git status, untuk melihat status file yang ingin di upload
+Git remote, digunakan untuk melakukan remote terhadapa repo bersifat online
+Git push, berfungsi untuk melakukan proses upload.
--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-/* document.getElementsByClassName("floating-menu").addEventListener("click", displayDate);
-function displayDate() {
-  console.log("klik");
-} */
+var menuButtonStatus = false;
+
+function mainMenuButton(){
+  var menuList = document.querySelectorAll(".module");
+  if (menuButtonStatus === false) {
+    for (let i = 0; i < menuList.length; i++) {
+      menuList[i].style.display = "block";
+    }
+    menuButtonStatus = true;
+  }
+  else if (menuButtonStatus === true) {
+    for (let i = 0; i < menuList.length; i++) {
+      menuList[i].style.display = "none";
+    }
+    menuButtonStatus = false;
+  }
+}
 
 var dropdown = document.getElementsByClassName("module-name");
 var i;
@@ -15,4 +28,14 @@ for (i = 0; i < dropdown.length; i++) {
   dropdownContent.style.display = "block";
   }
   });
+}
+
+
+
+var f = document.getElementById("angka").textContent;
+console.log(f);
+var x = pengurangan(f);
+document.getElementById("hasil").innerHTML = x;
+function pengurangan (a){
+  return a-2;
 }
